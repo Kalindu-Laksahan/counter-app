5d75f7b9743ef24c48f9c02385d3e9f5d6c75fa3
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2023-07-13 11:56:34 +0530

    Added first component

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'   //imrc
+
+//cc
+class Counter extends Component {
+   
+    render() { 
+        return <h1>Hello World</h1>;
+    }
+}
+ 
+export default Counter;
