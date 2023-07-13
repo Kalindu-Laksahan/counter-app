878dca8403526f5b0a351ec35a44dc01a41dc2e9
Kalindu Laksahan <68551659+Kalindu-Laksahan@users.noreply.github.com>
2023-07-13 15:37:51 +0530

    modified increment button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,6 +8,10 @@ class Counter extends Component {
         count: 0,
         tags:['tag1','tag2','tag3']
     };
+    handleIncrement= product =>{
+        console.log(product);
+        this.setState({count: this.state.count + 1});
+    }
    
     render() { 
         return (
@@ -15,7 +19,7 @@ class Counter extends Component {
         <div>
             
             <span className={this.setBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+            <button onClick={(product) => this.handleIncrement(product)} className='btn btn-secondary btn-sm'>Increment</button>
 
             <ul>
                 {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
@@ -39,10 +43,9 @@ class Counter extends Component {
         classes += this.state.count === 0 ? 'warning' : 'primary';
         return classes;
     }
-    handleIncrement=() =>{
-        
-        this.setState({count: this.state.count + 1});
-    }
+    
+
+    
 
 }
  
